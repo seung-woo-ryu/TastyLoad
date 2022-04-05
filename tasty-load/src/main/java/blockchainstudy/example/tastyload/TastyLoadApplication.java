f0dc2e1b1d52b856469ed0f055c2cbbc6e4a133a
seung-woo-ryu <tmddn645@naver.com>
2022-04-05 14:41:24 +0900

    spring boot starter

---
+++ tasty-load/src/main/java/blockchainstudy/example/tastyload/TastyLoadApplication.java
@@ -0,0 +1,13 @@
+package blockchainstudy.example.tastyload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyLoadApplication.class, args);
+	}
+
+}
---
+++ tasty-load/src/test/java/blockchainstudy/example/tastyload/TastyLoadApplicationTests.java
@@ -0,0 +1,13 @@
+package blockchainstudy.example.tastyload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyLoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
